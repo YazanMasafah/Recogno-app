eb2e2d6e24a5ba0c071c4b0f4f3c703aa9c36b00
YazanMasafah <yazan.masafah@gmail.com>
2020-05-21 01:56:53 +0300

    update port to 3000

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
   }
 
   componentDidMount(){
-    fetch('http://localhost:4000/')
+    fetch('http://localhost:3000/')
     .then(response => response.json())
     .then(console.log)
   }
